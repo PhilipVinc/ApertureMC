18f19a03175c95d15e242d706ee8d456e7ee3e4f
Filippo Vicentini <filippovicentini@gmail.com>
2014-03-22 15:29:05 +0100

    Initial Commit

---
+++ DataElaborator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataElaborator
+//
+//  Created by Filippo Vicentini on 22/03/14.
+//  Copyright (c) 2014 Filippo Vicentini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
